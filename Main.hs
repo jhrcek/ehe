052a121973677aea3e7cd328fef0b9511ecc9165
Jan Hrcek <honza.hrk@gmail.com>
2017-06-17 20:35:36 +0200

    Just extract json data from errai posts (to be rendered by elm)

--- Main.hs
+++ Main.hs
@@ -1,111 +1,89 @@
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
 
+import System.Directory
 import Data.Aeson
---import qualified Data.ByteString.Lazy as B
 import Data.Aeson.Encode.Pretty (encodePretty)
 import qualified Data.ByteString.Lazy.Char8 as B
-import Data.Either (partitionEithers)
-import Data.Maybe (catMaybes, fromJust)
+import Data.Maybe (fromJust)
 import Data.Text (Text)
+import Data.Foldable (traverse_)
 import qualified Data.Text as T
 import Data.Text.Encoding (encodeUtf8)
 import GHC.Generics
 import Prelude hiding (log)
 import System.Environment (getArgs)
 import System.Exit (die)
+import qualified Data.Vector as V
+import Control.Monad (when)
 
 main :: IO ()
 main = do
-  infile <- parseArgs
+  Config infile debug <- parseArgs
   harBytes <- B.readFile infile
   let eHar = eitherDecode harBytes :: Either String Har
-  either processParseError processHar eHar
+  either processParseError (processHar debug) eHar
+
+data Config = Config FilePath Bool
 
 processParseError :: String -> IO ()
 processParseError err = die $ "Invalid HAR file: " ++ err
 
-processHar :: Har -> IO ()
-processHar har = do
-    -- TODO how to present extracted errai messages?
-    -- 1) pretty jsons -> too much data
-    mapM_ (maybe (return ()) B.putStrLn) prettyErraiJsons
-
-    -- 2) fixed fields -> but don't know which are there and which are relevant
-    mapM_ print erraiMessages
-
-    printStats harEntries erraiPostEntries errors erraiMessages
+processHar :: Bool -> Har -> IO ()
+processHar debug har = do
+    when debug $ traverse_ (B.putStrLn . encodePretty) vectorOfErraiMessages
+    let allMessagesByteString = encode $ Array vectorOfErraiMessages
+        stringEncodedValue = show allMessagesByteString
+        -- TODO write to elm index file
+    printStats harEntries erraiPostEntries vectorOfErraiMessages
   where
     harEntries = entries $ log har
     erraiPostEntries = filter isErraiEntry harEntries
+    vectorOfErraiMessages = V.concat $ fmap getMessageObjectsFromErraiMessageJson erraiPostEntries
 
-    prettyErraiJsons = fmap (erraiPostBodyToPrettyJson . erraiMessagesJson) erraiPostEntries
+printStats :: [HarEntry] -> [HarEntry] -> V.Vector Value -> IO ()
+printStats allHarEntries erraiPostEntries erraiMessages = do
+    putStrLn $ "HAR had " ++ show (length allHarEntries ) ++ " entries"
+    putStrLn $ "Out of that " ++ show (length erraiPostEntries) ++ " were Errai POST entries"
+    putStrLn $ "Errai POST entries contained " ++ show (V.length erraiMessages) ++ " errai messages"
 
-    (errors, listsOfMessages) = partitionEithers $ fmap extractErraiMessages erraiPostEntries
-    erraiMessages = concat listsOfMessages
-
-printStats :: [HarEntry] -> [HarEntry] -> [String] -> [ErraiMessage] -> IO ()
-printStats allHarEntries erraiPostEntries erraiErrors erraiMsgs = do
-    putStrLn $ "Total entries : " ++ shoLen allHarEntries ++ " (" ++ shoLen erraiPostEntries ++ " errai POST entries)"
-    putStrLn $ "Errai messages: " ++ shoLen erraiMsgs ++ " decoded successfully, " ++ shoLen erraiErrors ++ " failed to decode"
-  where
-    shoLen = show . length
-
-erraiPostBodyToPrettyJson :: Text -> Maybe B.ByteString
-erraiPostBodyToPrettyJson tbody =
-    fmap encodePretty ((decode $ toLbs tbody) :: Maybe Value)
-
-erraiMessagesJson :: HarEntry -> Text
+erraiPostToJsonText :: HarEntry -> Text
 -- fromJust justified because we are sure there's postData based on isErraiEntry
-erraiMessagesJson = text . fromJust . postData . request
+erraiPostToJsonText = text . fromJust . postData . request
 
-extractErraiMessages :: HarEntry -> Either String [ErraiMessage]
-extractErraiMessages = eitherDecode . toLbs . erraiMessagesJson
+getMessageObjectsFromErraiMessageJson :: HarEntry -> V.Vector Value
+getMessageObjectsFromErraiMessageJson entry = array
+  where
+    eitherValue = eitherDecode . toLbs $ erraiPostToJsonText entry
+    val = either ( \err -> error $ "Errai message body didn't contain valid JSON\n  error was: " ++ err ++ "\n  body was:\n" ++ show entry) id eitherValue
+    array = case val of
+        Array ar -> ar
+        _ -> error $ "Errai POST's body's top level value was not array: " ++ show entry
 
-parseArgs :: IO FilePath
+
+parseArgs :: IO Config
 parseArgs = do
   as <- getArgs
   case as of
     []    -> die "Usage ehe <file.har>"
-    (f:_) -> return f
+    (file:remainingArgs) ->
+      Config <$> verifyFile file <*> pure (isDebug remainingArgs)
+
+verifyFile :: FilePath -> IO FilePath
+verifyFile file = do
+  exists <- doesFileExist file
+  if exists
+    then return file
+    else die $ "File " ++ file ++ " does not exist\nUsage ehe <file.har>"
+
+isDebug :: [String] -> Bool
+isDebug ("--debug":_) = True
+isDebug _ = False
 
 toLbs :: T.Text -> B.ByteString
 toLbs = B.fromStrict . encodeUtf8
 ------------
-data ErraiMessage = ErraiMessage
-  { toSubject          :: Text
-  , commandType        :: Maybe Text
-  , replyTo            :: Maybe Text
-  , value              :: Maybe Text
-  , priorityProcessing :: Maybe Text
-  , errorMessage       :: Maybe Text
-  , throwable          :: Maybe Text
-  }
 
-instance Show ErraiMessage where
-  show ErraiMessage{..} =
-    T.unpack . T.unlines $
-       catMaybes [Just "Errai Message"
-       , may "ToSubject" (Just toSubject)
-       , may "CommandType" commandType
-       -- , may "ReplyTo" replyTo
-       , may "Value"  value
-       , may "PriorityProcessing" priorityProcessing
-       , may "ErrorMessage" errorMessage
-       , may "Throwable" throwable
-       ]
-       where may t = fmap (\v -> T.concat ["  ", t, " = ", v])
-
-instance FromJSON ErraiMessage where
-  parseJSON = withObject "ErraiMessage" $ \v -> ErraiMessage
-        <$> v .: "ToSubject"
-        <*> v .:? "CommandType"
-        <*> v .:? "ReplyTo"
-        <*> v .:? "Value"
-        <*> v .:? "PriorityProcessing"
-        <*> v .:? "ErrorMessage"
-        <*> v .:? "Throwable"
 
 newtype Har = Har
   { log :: Entries
